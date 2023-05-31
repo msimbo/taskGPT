7308612310bb88e0c77d8dae0ca075473a0a93c7
Dele Omotosho <dele@authoritywit.com>
2023-05-31 14:07:10 -0400

    adds the starting point of JS

---
+++ js/app.js
@@ -0,0 +1,58 @@
+// C-style
+
+// get that HTML section into JS
+const pomoBtn = document.getElementById("pomo-btn");
+const matrixBtn = document.getElementById("matrix-btn");
+
+console.log(pomoBtn);
+console.log(matrixBtn);
+
+// console.log("deed")
+
+// 1. define the problem
+// 1a. when a user clicks one of my divs
+
+// const myBtn = "stuff";
+// const myName = "Dele Tosh deedeed";
+// const myPlace = "ULEM";
+// const myNumOne = "1"; //string
+// const myNumTwo = "2";
+
+// const myNumberOne = 1; //number
+// const myNumberTwo = 2;
+
+// const isThisHere = true; //boolean
+// const isItNotHere = false;
+
+// const firstTotal = myNumOne + myNumTwo;
+// const secondTotal = myNumberOne + myNumberTwo;
+
+// const multiplyResult = myNumberOne * myNumberTwo;
+
+// console.log(isThisHere);
+
+// console.log(secondTotal);
+
+// console.log(multiplyResult);
+
+// console.log(myPlace);
+
+// console.log(typeof isThisHere);
+// console.log(typeof myNumTwo);
+// console.log(typeof myNumberOne);
+
+// console.log( myNumberOn)
+// variable called "myBtn" that has "stuff"
+
+// console.log(myPlace);
+// console.log(myNumberOne);
+// console.log(myPlace);
+// console.log(myBtn);
+
+// 1b i see a pop-up
+
+// const newName = prompt("What is your name?");
+
+// console.log(newName);
+
+// console.log(typeof newName);
